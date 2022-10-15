0ed7598815845988cd307a7898cef3a07b86b908
ThierryM1212 <thierry.mangiante@gmail.com>
2022-10-16 00:44:49 +0200

    upgrade to react-scripts 5 to upgrade the dependencies with security issues

--- front-end/config-overrides.js
+++ front-end/config-overrides.js
@@ -1,15 +1,23 @@
 const path = require('path');
+const webpack = require('webpack');
+process.env.GENERATE_SOURCEMAP = 'false';
 
 module.exports = function override(config, env) {
   const wasmExtensionRegExp = /\.wasm$/;
 
-  config.resolve.extensions.push('.wasm');
-
-  config.module.rules.forEach(rule => {
-    (rule.oneOf || []).forEach(oneOf => {
-      if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
-        // make file-loader ignore WASM files
-        oneOf.exclude.push(wasmExtensionRegExp);
+  // Resolve WASM and CommonJS
+  config.resolve.extensions.push(".wasm");
+  config.experiments = {
+    asyncWebAssembly: true,
+  };
+  config.module.rules.forEach((rule) => {
+    (rule.oneOf ?? []).forEach((oneOf) => {
+      if (oneOf.type === "asset/resource") {
+        // Including .cjs here solves `nanoid is not a function`
+        oneOf.exclude.push(/\.wasm$/, /\.cjs$/);
+      } else if (new RegExp(oneOf.test).test(".d.ts")) {
+        // Exclude declaration files from being loaded by babel
+        oneOf.exclude = [/\.d\.ts$/];
       }
     });
   });
@@ -20,6 +28,24 @@ module.exports = function override(config, env) {
     include: path.resolve(__dirname, 'src'),
     use: [{ loader: require.resolve('wasm-loader'), options: {} }]
   });
+
+  const fallback = config.resolve.fallback || {};
+  Object.assign(fallback, {
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify"),
+    "url": require.resolve("url")
+  })
+  config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ])
 
   return config;
 };
